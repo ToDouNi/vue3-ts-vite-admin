dc16e09acbce38f007497ce21acf6ce3483ba6a3
Todou <todouni@163.com>
2022-04-27 09:47:43 +0800

    update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,15 @@
 <script setup lang='ts'>
-import { useStore } from '../../store'
+import { useStore } from '@/store'
 const store = useStore()
 console.log(store.state.count)
 </script>
 
 <template>
-  <div>这是home {{ $store.state.count }}</div>
+  <div class="container">这是home {{ store.state.count }}</div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.container{
+  background: $redColor;
+}
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -8,5 +9,17 @@ export default defineConfig({
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/variables.scss";'
+      }
+    }
+  }
 })
